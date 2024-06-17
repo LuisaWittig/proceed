5caee217024fb45a3c3d04580ff5935dc809f6ff
Chinchila <pachecosobral@gmail.com>
2024-06-18 01:13:38 +0200

    fix+refactor: treeview now is an element

--- src/management-system-v2/lib/data/legacy/machine-config.ts
+++ src/management-system-v2/lib/data/legacy/machine-config.ts
@@ -112,7 +112,7 @@ export async function createMachineConfig(
     const metadata: MachineConfig = {
       ...({
         id: v4(),
-        type: 'machine-config',
+        type: 'config',
         name: 'Default Machine Configuration',
         description: '',
         variables: [],
